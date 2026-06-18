fc211449495b6e8fc4a3459678fc69e5840728b3
Sara <sara.beggiato.90@gmail.com>
2026-06-18 09:56:13 +0200

    Update streamlit_plygr.py
    applying st.write()

--- code/streamlit playground/streamlit_plygr.py
+++ code/streamlit playground/streamlit_plygr.py
@@ -4,11 +4,16 @@ in terminal run: streamlit run your_script.py """
 import streamlit as st
 import pandas as pd
 
+
+# With st.write, you can write just about anything to the app: text, data, Matplotlib figures, Altair charts and more
+
+st.write("Hello, Streamlit! This is a simple app to demonstrate some of its features.")
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
-df
+df      # writing a variable on a separate line, automatically displays it on the app!
 
-# writing a variable, automatically displays it on the app! It's almost magic XD
+
