d1b7aac561d24728e9912d17824e4ecd57adc117
Sara <sara.beggiato.90@gmail.com>
2026-07-01 10:46:38 +0200

    Update streamlit_plygr.py

--- code/streamlit playground/streamlit_plygr.py
+++ code/streamlit playground/streamlit_plygr.py
@@ -48,3 +48,15 @@ add_selectbox = st.sidebar.selectbox(
     ('Email', 'Home phone', 'Mobile phone')
 )
 add_input_number = st.sidebar.number_input('Insert the number of people working this month', min_value=1, max_value=10, value=1, step=1)
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
