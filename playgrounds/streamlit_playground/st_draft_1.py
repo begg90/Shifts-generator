740ece475df5e60464498d3db038c8c6dc5c4788
Sara <sara.beggiato.90@gmail.com>
2026-09-15 10:27:25 +0200

    Update st_draft_1.py
    setting the home page and the sidebar

--- playgrounds/streamlit_playground/st_draft_1.py
+++ playgrounds/streamlit_playground/st_draft_1.py
@@ -1,13 +1,23 @@
 import streamlit as st
 
-st.write("This is a first attempt at creating a draft of our app. It will be used to test some features and functions of Streamlit and get an idea of the " \
-        "things we will need to implement.")
+
+st.set_page_config(
+    page_title="Welcome!",
+    page_icon="👋",
+)
+
+# Welcome message on the home page
 
 st.title(":red[WELCOME TO OUR SHIFTS GENERATOR APP!]")
 
-st.write('Do you want to create a new shift?')
-if st.button('Yes'):
-    st.write('You clicked Yes!')
+st.write("This is a first attempt at creating a draft of our app. It will be used to test some features and functions of Streamlit and get an idea of the " \
+        "things we will need to implement.")
 
-pg = st.navigation([st.Page("pages/page_1.py"), st.Page("pages/page_2.py")], position="hidden")
-pg.run()
+
+# Working on the sidebar & navigation menu
+
+st.sidebar.info('What would you like to do?')
+
+
+new_shift_page = st.Page("pages/page_1.py", title="New Shift")
+pg = st.navigation([new_shift_page])
