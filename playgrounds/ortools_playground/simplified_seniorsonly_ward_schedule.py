9d39183a9a1873f2229bdfa600ef4564393a4beb
BeatriceSaggiorato <114568378+BeatriceSaggiorato@users.noreply.github.com>
2026-09-18 10:56:17 +0200

    refactoring: class based simplified model

--- playgrounds/ortools_playground/simplified_seniorsonly_ward_schedule.py
+++ playgrounds/ortools_playground/simplified_seniorsonly_ward_schedule.py
@@ -10,72 +10,67 @@
 from ortools.sat.python import cp_model
 #from ortools.sat.python import cp_model_helper
 
-# Data
-doctors = ["eenie",
-           "meenie",
-           "miney",
-           "moe",
-           "tom",
-           "jerry"]
-shift_types = ["NightDuty"]
-num_days = 30 # to mimic a month
-dates = list(range(num_days)) # NB this goes from 0, NOT like a calendar
+def get_data():
+    doctors = ["eenie",
+            "meenie",
+            "miney",
+            "moe",
+            "tom",
+            "jerry"]
+    shift_types = ["NightDuty"]
+    num_days = 30 # to mimic a month
+    dates = list(range(num_days)) # NB this goes from 0, NOT like a calendar
+    return doctors, dates, shift_types
 
-# CREATE MODEL
-model = cp_model.CpModel()
-model.clear_assumptions
+class scheduled_model(cp_model.CpModel):
+    """our model including variables and constraints"""
+    def __init__(self, doctors, dates, shift_types):
+        cp_model.CpModel.__init__(self)
+        self._doctors = doctors
+        self._dates = dates
+        self._shift_types = shift_types
+        self.shifts = {}
 
-# CREATE VARIABLES
-# a dictionary for now, a pandas series later
-shifts = {}
-for doc in doctors:
-    for day in dates:
-        for shift in shift_types:
-            shifts[(doc,day,shift)] = model.new_bool_var(f"shift_{doc}_day{day}_{shift}")
+    def create_variables(self):
+        # a dictionary for now, a pandas series later
+        for doc in self._doctors:
+            for day in self._dates:
+                for shift in self._shift_types:
+                    self.shifts[(doc,day,shift)] = self.new_bool_var(f"shift_{doc}_day{day}_{shift}")
+        return self.shifts
+    
+    def one_employee_per_shift(self):
+        #constraint
+        for day in self._dates:
+            for shift in self._shift_types:
+                self.add_exactly_one(self.shifts[doc,day,shift] for doc in self._doctors)
+        return
 
-#print(shifts)
+    def forbid_consecutive_shifts(self):
+        # constraint
+        for doc in self._doctors:
+            for shift in self._shift_types:
+                for day in range(len(self._dates)-1):
+                    self.add_at_most_one([self.shifts[doc,self._dates[day],shift],self.shifts[doc,self._dates[day+1],shift]])
+        return            
 
-# CREATE CONSTRAINTS
-# assign doctors to shifts, with rules:
-# 1. each shift is assigned to a single doctor
-# 2. no doctor works consecutive days
+    def evenly_distribute_workload(self):
+        # constraint
+        min_shifts_per_doctor = (len(self._dates) * len(self._shift_types)) // len(self._doctors)
+        if len(self._dates) * len(self._shift_types) % len(self._doctors) == 0:
+            max_shifts_per_doctor = min_shifts_per_doctor
+        else:
+            max_shifts_per_doctor = min_shifts_per_doctor + 1
+        for doc in self._doctors:
+            shifts_worked = []
+            for day in self._dates:
+                for shift in self._shift_types:
+                    shifts_worked.append(self.shifts[((doc,day,shift))])
+            self.add(min_shifts_per_doctor <= sum(shifts_worked))     
+            self.add(sum(shifts_worked) <= max_shifts_per_doctor)
+        return    
 
-# Constraint: assign each shift to a single doctor
-for day in dates:
-    for shift in shift_types:
-        model.add_exactly_one(shifts[doc,day,shift] for doc in doctors)
-#print(model)
 
-# Constraint: doctors do not work consecutive shifts
-for doc in doctors:
-    for shift in shift_types:
-        for day in range(len(dates)-1):
-            model.add_at_most_one([shifts[doc,dates[day],shift],shifts[doc,dates[day+1],shift]])
-#print(model)
-
-# Constraint: the shifts are evenly ditributed among doctors
-min_shifts_per_doctor = (len(dates) * len(shift_types)) // len(doctors)
-if len(dates) * len(shift_types) % len(doctors) == 0:
-    max_shifts_per_doctor = min_shifts_per_doctor
-else:
-    max_shifts_per_doctor = min_shifts_per_doctor + 1
-for doc in doctors:
-    shifts_worked = []
-    for day in dates:
-        for shift in shift_types:
-            shifts_worked.append(shifts[((doc,day,shift))])
-    model.add(min_shifts_per_doctor <= sum(shifts_worked))     
-    model.add(sum(shifts_worked) <= max_shifts_per_doctor)
-#print(model)
-
-# CREATE SOLVER
-# update solver parameters
-solver = cp_model.CpSolver()
-solver.parameters.linearization_level = 0
-# enumerate all solutions
-solver.parameters.enumerate_all_solutions = True
-
-# register a solution callback
 class doctorsPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
     """print intermediate solutions"""
 
@@ -91,7 +86,7 @@ class doctorsPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
     def on_solution_callback(self):
         self._solution_count += 1
         print(f"Solution {self._solution_count}")
-        for day in dates:
+        for day in self._dates:
             print(f"Day {day}")
             for doc in self._doctors:
                 is_working = False
@@ -109,10 +104,33 @@ class doctorsPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
         return self._solution_count
 
 
-# Display the first five solutions.
-solution_limit = 5
-solution_printer = doctorsPartialSolutionPrinter(
-    shifts, doctors, dates, shift_types, solution_limit)
 
-# INVOKE THE SOLVER
-solver.solve(model, solution_printer)
+
+def main() -> None:
+    # data
+    [doctors,dates,shift_types] = get_data()
+    # create model
+    model = scheduled_model(doctors, dates, shift_types)
+    model.create_variables()
+    model.one_employee_per_shift()
+    model.forbid_consecutive_shifts()
+    model.evenly_distribute_workload()
+    
+    # create model
+    solver = cp_model.CpSolver()
+    solver.parameters.linearization_level = 0
+
+    # enumerate all solutions
+    solver.parameters.enumerate_all_solutions = True
+
+    # Display the first five solutions.
+    solution_limit = 5
+    solution_printer = doctorsPartialSolutionPrinter(
+        model.shifts, doctors, dates, shift_types, solution_limit)
+
+    # invoke the solver
+    solver.solve(model, solution_printer)
+
+
+if __name__ == "__main__":
+    main()
