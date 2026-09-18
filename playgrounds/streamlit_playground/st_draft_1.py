65645f6c827d5c768fd0b62ca686c3ae8d36a201
Sara <sara.beggiato.90@gmail.com>
2026-09-18 10:44:56 +0200

    Multipage setup
    Finished setting up the (dummy for now) log-in and organizing the first couple pages.

--- playgrounds/streamlit_playground/st_draft_1.py
+++ playgrounds/streamlit_playground/st_draft_1.py
@@ -1,18 +1,17 @@
 import streamlit as st
 
+# Setting a dummy logged_in state for testing purposes. The login funcion is also the welcome page of the app. 
+# The user will be able to log in and log out, and the navigation will change accordingly.
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
-# Welcome message on the home page
-
-st.title(":red[WELCOME TO OUR SHIFTS GENERATOR APP!]")
-
-st.write("This is a first attempt at creating a draft of our app. It will be used to test some features and functions of Streamlit and get an idea of the " \
-        "things we will need to implement.")
-
 
 def login():
+    st.title(":red[WELCOME TO OUR SHIFTS GENERATOR APP!]")
+    st.write("This is a first attempt at creating a draft of our app. It will be used to test some features and functions of Streamlit and get an idea of the " \
+        "things we will need to implement.")
+
     if st.button("Log in"):
         st.session_state.logged_in = True
         st.rerun()
@@ -21,6 +20,9 @@ def logout():
     if st.button("Log out"):
         st.session_state.logged_in = False
         st.rerun()
+
+
+# Defining the pages of the app. Until  the users log in, he won't be able to access the other pages, even if he tries to navigate to them directly. 
 
 
 login_page = st.Page(login, title="Log in", icon=":material/login:")
